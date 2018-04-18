1bc5d09fa8979187952cff083b35fc5b2382628e
EParedez <emanuellpp@gmail.com>
2018-04-17 23:38:52 -0500

    json file, jquery lib, list products

--- wwwroot/js/app/products.js
+++ wwwroot/js/app/products.js
@@ -1,12 +1,17 @@
 
 new Vue({
-    'el':'body',
+    'el':'.container-fluid',
     data: function () {
         return {
-
+            products: []
         }
     },
     created: function () {
         console.log('created')
+        var context = this;
+        $.get('./../wwwroot/data/products.json', {}, function (datas) {
+            console.log(datas)
+            context.products = datas.data.products
+        });
     }
 });
